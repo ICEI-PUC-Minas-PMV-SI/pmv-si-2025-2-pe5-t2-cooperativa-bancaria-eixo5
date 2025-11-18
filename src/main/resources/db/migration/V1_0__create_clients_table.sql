3f6491586743ccca323881c77caa510bcc16d1db
Vinicius Neves <vinicius.neeves@outlook.com>
2025-11-18 14:59:19 -0300

    feat: adjusted code

--- src/main/resources/db/migration/V1_0__create_clients_table.sql
+++ src/main/resources/db/migration/V1_0__create_clients_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE clients (
-    id VARCHAR(250) PRIMARY KEY,
-    account INTEGER UNIQUE,
-    agency INTEGER NOT NULL,
-    name VARCHAR(200) NOT NULL,
-    email VARCHAR(200) NOT NULL,
-    password INTEGER NOT NULL
-);
+  CREATE TABLE clients (
+      id VARCHAR(250) PRIMARY KEY,
+      account INTEGER UNIQUE,
+      agency INTEGER NOT NULL,
+      name VARCHAR(200) NOT NULL,
+      email VARCHAR(200) NOT NULL,
+      password INTEGER NOT NULL
+  );
